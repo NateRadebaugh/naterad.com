88b6989424f7bcdb6a96e8af38a60858f6e086c4
Nate Radebaugh <nate.radebaugh@outlook.com>
2019-04-26 21:54:52 -0500

    Add now-offline package

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const withOffline = require("next-offline");
 const withTypescript = require("@zeit/next-typescript");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const withMDX = require("@zeit/next-mdx")({
@@ -7,22 +8,45 @@ const withMDX = require("@zeit/next-mdx")({
     hastPlugins: []
   }
 });
-module.exports = withTypescript(
-  withMDX({
-    target: "serverless",
-    pageExtensions: ["js", "jsx", "md", "mdx"],
-    exportPathMap: function() {
-      return {
-        "/": { page: "/" }
-      };
-    },
-    webpack(config, options) {
-      // Do not run type checking twice:
-      if (options.isServer) {
-        config.plugins.push(new ForkTsCheckerWebpackPlugin());
-      }
 
-      return config;
-    }
-  })
+module.exports = withOffline(
+  withTypescript(
+    withMDX({
+      target: "serverless",
+      pageExtensions: ["js", "jsx", "md", "mdx"],
+      exportPathMap: function() {
+        return {
+          "/": { page: "/" }
+        };
+      },
+      webpack(config, options) {
+        // Do not run type checking twice:
+        if (options.isServer) {
+          config.plugins.push(new ForkTsCheckerWebpackPlugin());
+        }
+
+        return config;
+      },
+      workboxOpts: {
+        swDest: "static/service-worker.js",
+        runtimeCaching: [
+          {
+            urlPattern: /^https?.*/,
+            handler: "networkFirst",
+            options: {
+              cacheName: "https-calls",
+              networkTimeoutSeconds: 15,
+              expiration: {
+                maxEntries: 150,
+                maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      }
+    })
+  )
 );
