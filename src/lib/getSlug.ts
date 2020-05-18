aea7b8474da2dc8be28e422000e2cb06b4e0eab3
Nate Radebaugh <nate.radebaugh@outlook.com>
2020-05-17 20:28:53 -0500

    Add getSlug lib file

---
+++ src/lib/getSlug.ts
@@ -0,0 +1,21 @@
+function getSlug(resourcePath) {
+  {
+    const matches = resourcePath.match(/([^\\/]+)\/index\.mdx$/);
+    if (matches) {
+      const slug = matches[1];
+
+      return slug;
+    }
+  }
+
+  {
+    const matches = resourcePath.match(/([^\\/]+)\.mdx$/);
+    if (matches) {
+      const slug = matches[1];
+
+      return slug;
+    }
+  }
+}
+
+export default getSlug;
