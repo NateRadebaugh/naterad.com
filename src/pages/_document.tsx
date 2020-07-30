2eca528c6a6a82eccaf38369b95a1d22ed2ee124
Nate Radebaugh <nate.radebaugh@outlook.com>
2020-07-30 06:43:28 -0500

    Move viewport meta from _document to _app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,10 +8,6 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icons-192.png" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
         </Head>
         <body>
           <Main />
