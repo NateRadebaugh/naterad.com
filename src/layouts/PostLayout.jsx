e9cff4ba0544d37b01ece08e4b13516bc517ba6a
Nate Radebaugh <nradebaugh@bdo.com>
2020-08-26 19:02:35 -0500

    Bump to latest and react to breaking next-mdx-enhanced

--- src/layouts/PostLayout.jsx
+++ src/layouts/PostLayout.jsx
@@ -10,7 +10,7 @@ import { MDXProvider } from "@mdx-js/react";
 import styles from "../styles/syntax.module.scss";
 import postStyles from "./PostLayout.module.scss";
 
-function PostLayout(frontMatter) {
+function PostLayout({ children, frontMatter }) {
   const slug = getSlug(frontMatter.__resourcePath);
   const posts = docsPages;
 
@@ -45,61 +45,59 @@ function PostLayout(frontMatter) {
   const nextTitle = nextPost?.title ?? null;
   const nextDate = nextPost?.date ?? null;
 
-  return ({ children }) => {
-    return (
-      <BlogLayout {...frontMatter} isPost>
-        <MDXProvider components={mdxComponents}>
-          <div className={styles.syntax}>
-            <div className={postStyles.post}>{children}</div>
-          </div>
-        </MDXProvider>
+  return (
+    <BlogLayout {...frontMatter} isPost>
+      <MDXProvider components={mdxComponents}>
+        <div className={styles.syntax}>
+          <div className={postStyles.post}>{children}</div>
+        </div>
+      </MDXProvider>
 
-        <Stack direction="vertical" gap={3}>
-          <Divider />
+      <Stack direction="vertical" gap={3}>
+        <Divider />
 
-          <Heading size="paragraph">
-            <Text variant="subtle">Further reading...</Text>
-          </Heading>
+        <Heading size="paragraph">
+          <Text variant="subtle">Further reading...</Text>
+        </Heading>
 
-          <Stack justify="space-between">
-            {hasPrev ? (
-              <strong style={{ width: "50%" }}>
-                <Link href="/blog/[slug]" as={`/blog/${prevSlug}`}>
-                  <div css="display: inline-block">
-                    &laquo; {prevTitle}
-                    <br />
-                    <Text variant="subtle" size={2}>
-                      {prevDate.format("MMMM D, YYYY h:mm A")}
-                    </Text>
-                  </div>
-                </Link>
-              </strong>
-            ) : (
-              <div />
-            )}
+        <Stack justify="space-between">
+          {hasPrev ? (
+            <strong style={{ width: "50%" }}>
+              <Link href="/blog/[slug]" as={`/blog/${prevSlug}`}>
+                <div css="display: inline-block">
+                  &laquo; {prevTitle}
+                  <br />
+                  <Text variant="subtle" size={2}>
+                    {prevDate.format("MMMM D, YYYY h:mm A")}
+                  </Text>
+                </div>
+              </Link>
+            </strong>
+          ) : (
+            <div />
+          )}
 
-            {hasNext ? (
-              <strong style={{ width: "50%", textAlign: "right" }}>
-                <Link href="/blog/[slug]" as={`/blog/${nextSlug}`}>
-                  <div css="display: inline-block">
-                    {nextTitle} &raquo;
-                    <br />
-                    <Text size={2} variant="subtle">
-                      {nextDate.format("MMMM D, YYYY h:mm A")}
-                    </Text>
-                  </div>
-                </Link>
-              </strong>
-            ) : (
-              <div />
-            )}
-          </Stack>
-
-          <Divider marginTop={1} marginBottom={3} />
+          {hasNext ? (
+            <strong style={{ width: "50%", textAlign: "right" }}>
+              <Link href="/blog/[slug]" as={`/blog/${nextSlug}`}>
+                <div css="display: inline-block">
+                  {nextTitle} &raquo;
+                  <br />
+                  <Text size={2} variant="subtle">
+                    {nextDate.format("MMMM D, YYYY h:mm A")}
+                  </Text>
+                </div>
+              </Link>
+            </strong>
+          ) : (
+            <div />
+          )}
         </Stack>
-      </BlogLayout>
-    );
-  };
+
+        <Divider marginTop={1} marginBottom={3} />
+      </Stack>
+    </BlogLayout>
+  );
 }
 
 export default PostLayout;
