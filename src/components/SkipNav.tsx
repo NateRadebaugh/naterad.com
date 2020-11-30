22622a4e6dd62a5d54fe08e2c979525a6c9fc68e
Nate Radebaugh <nradebaugh@bdo.com>
2020-11-30 09:00:24 -0600

    Strip out extra code from @reach/skip-nav for smaller bundle

---
+++ src/components/SkipNav.tsx
@@ -0,0 +1,20 @@
+// Copied and stripped from https://github.com/reach/reach-ui/blob/develop/packages/skip-nav/src/index.tsx
+
+import * as React from "react";
+
+function SkipNavLink() {
+  return (
+    <a href="#skip-nav" data-skip-nav-link="">
+      Skip to content
+    </a>
+  );
+}
+
+function SkipNavContent() {
+  return <div id="skip-nav" data-skip-nav-content=""></div>;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Exports
+
+export { SkipNavLink, SkipNavContent };
