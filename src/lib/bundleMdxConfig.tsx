eb0ee195874aeb801e5e20a6c78b4e0484be9d51
Nate Radebaugh <nradebaugh@bdo.com>
2021-05-19 13:15:42 -0500

    Fix mdx-bundler type break

--- src/lib/bundleMdxConfig.tsx
+++ src/lib/bundleMdxConfig.tsx
@@ -1,13 +1,6 @@
-const bundleMdxConfig = {
-  xdmOptions(input, options) {
-    // this is the recommended way to add custom remark/rehype plugins:
-    // The syntax might look weird, but it protects you in case we add/remove
-    // plugins in the future.
-    // options.remarkPlugins = [...(options.remarkPlugins ?? []), myRemarkPlugin];
-    // options.rehypePlugins = [...(options.rehypePlugins ?? []), myRehypePlugin];
+import { BundleMDXOptions } from "mdx-bundler/dist/types";
 
-    return options;
-  },
+const bundleMdxConfig: BundleMDXOptions = {
 };
 
 export default bundleMdxConfig;
