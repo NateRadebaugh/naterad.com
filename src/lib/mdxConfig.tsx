355e5ea4138ab445d85ce5c4e33d52150cfc0562
Nate Radebaugh <nradebaugh@bdo.com>
2021-06-15 10:34:55 -0500

    Replace custom eslint setup with recommended next eslint config

--- src/lib/mdxConfig.tsx
+++ src/lib/mdxConfig.tsx
@@ -5,23 +5,29 @@ import Image from "next/image";
 
 const mdxConfig = {
   components: {
-    pre: (props) => <div {...props} />,
+    pre: function PreTag(props) {
+      return <div {...props} />;
+    },
     code: Code,
     a: Link,
     img: Image,
-    p: (props) => <p className="mb-3" {...props} />,
-    ol: (props) => (
-      <ul className="pl-3 mb-3 list-decimal list-outside" {...props} />
-    ),
-    ul: (props) => (
-      <ul className="pl-3 mb-3 list-disc list-outside" {...props} />
-    ),
-    blockquote: ({ style, ...props }) => (
-      <blockquote
-        className={clsx("pl-3 pt-1 border-l-4 border-gray-500")}
-        {...props}
-      />
-    ),
+    p: function PTag(props) {
+      return <p className="mb-3" {...props} />;
+    },
+    ol: function OlTag(props) {
+      return <ul className="pl-3 mb-3 list-decimal list-outside" {...props} />;
+    },
+    ul: function UlTag(props) {
+      return <ul className="pl-3 mb-3 list-disc list-outside" {...props} />;
+    },
+    blockquote: function BlockquoteTag({ style, ...props }) {
+      return (
+        <blockquote
+          className={clsx("pl-3 pt-1 border-l-4 border-gray-500")}
+          {...props}
+        />
+      );
+    },
   },
 };
 
