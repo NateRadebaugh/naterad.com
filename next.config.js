70bcf41f42deb26fcd03752f1db5b9e226e45fec
Nate Radebaugh <nradebaugh@bdo.com>
2021-06-18 07:36:47 -0500

    Remove experimental reactRoot config since it comes for free with latest next11

--- next.config.js
+++ next.config.js
@@ -31,9 +31,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 module.exports = withPlugins([withPWA, withBundleAnalyzer], {
   reactStrictMode: true,
-  experimental: {
-    reactRoot: true,
-  },
   i18n: {
     locales: ["en-US"],
     defaultLocale: "en-US",
