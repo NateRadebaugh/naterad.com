2ca06f0ecb9884c19a4dc551593855f1d6fcebac
Nate Radebaugh <nradebaugh@bdo.com>
2021-10-31 21:52:06 -0500

    Add remark-gfm

--- src/lib/bundleMdxConfig.tsx
+++ src/lib/bundleMdxConfig.tsx
@@ -1,4 +1,5 @@
 import { BundleMDXOptions } from "mdx-bundler/dist/types";
+import remarkGfm from "remark-gfm";
 import rehypeCodeTitles from "rehype-code-titles";
 import rehypePrism from "rehype-prism-plus";
 
@@ -7,7 +8,8 @@ const bundleMdxConfig: BundleMDXOptions = {
     // this is the recommended way to add custom remark/rehype plugins:
     // The syntax might look weird, but it protects you in case we add/remove
     // plugins in the future.
-    //options.remarkPlugins = [...(options.remarkPlugins ?? []), myRemarkPlugin];
+    options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkGfm];
+
     options.rehypePlugins = [
       ...(options.rehypePlugins ?? []),
       rehypeCodeTitles,
