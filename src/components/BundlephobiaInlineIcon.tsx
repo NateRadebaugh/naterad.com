ed19a5eb082518ab744f3a4acdab795b77b12407
Nate Radebaugh <nradebaugh@bdo.com>
2021-11-04 20:15:26 -0500

    Tweaks to home page

--- src/components/BundlephobiaInlineIcon.tsx
+++
@@ -1,8 +0,0 @@
-import Icon, { IconProps } from "./Icon";
-import { faCube } from "@fortawesome/free-solid-svg-icons/faCube";
-
-function BundlephobiaInlineIcon(props: Omit<IconProps, "icon">) {
-  return <Icon icon={faCube} {...props} />;
-}
-
-export default BundlephobiaInlineIcon;
