372fdb52bad5a1b4ca5ea1b470598dd2d4db68b4
Nate Radebaugh <nradebaugh@bdo.com>
2022-09-03 09:40:31 -0400

    Update PWA config in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@ const path = require("path");
 
 const withPlugins = require("next-compose-plugins");
 const withPWA = require("next-pwa")({
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    dest: "public",
-    sw: "service-worker.gen.js",
-  },
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
+  sw: "service-worker.gen.js",
 });
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
