b40d3a9d1551a7d41fef807ce2858156472d807a
Nate Radebaugh <nradebaugh@bdo.com>
2022-10-25 17:11:21 -0500

    Remove with-pwa for now since not supported in next 13

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-const path = require("path");
-
-const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa")({
-  disable: process.env.NODE_ENV === "development",
-  dest: "public",
-  sw: "service-worker.gen.js",
-});
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withPlugins([withPWA, withBundleAnalyzer], {
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
