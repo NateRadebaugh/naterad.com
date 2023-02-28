d16b3d75823d7e245e6ab200c68227617a6afcb6
Nate Radebaugh <nradebaugh@bdo.com>
2023-02-28 05:03:09 -0600

    Bump deps, migrate to new next metadata (#1748)

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: `Blog Posts - Nate Radebaugh&apos;s Blog`,
+  description: "Blog Posts - Nate Radebaugh's Blog",
+  themeColor: "#317EFB",
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return children;
