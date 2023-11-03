79313f410ed2bfb4f874884a4a3e501d4ff99740
Nate Radebaugh <nradebaugh@bdo.com>
2023-11-02 19:31:20 -0500

    Bump next to 14

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   experimental: {
-    appDir: true,
     serverComponentsExternalPackages: ["mdx-bundler"],
   },
   images: {
